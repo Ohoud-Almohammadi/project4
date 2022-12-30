149467654595807e6a58735fb494d70426629d3d
Ohoud Almohammadi <ohoud.hrb@gmail.com>
2022-12-30 13:29:33 +0300

    new

---
+++ set_env.sh
@@ -0,0 +1,11 @@
+export POSTGRES_USERNAME=postgres
+export POSTGRES_PASSWORD=postgres
+export POSTGRES_HOST=database-1.chi0rp99krui.us-east-1.rds.amazonaws.com
+export POSTGRES_DB=postgres
+export AWS_BUCKET=arn:aws:s3:::ohoud495892423351
+export AWS_REGION=us-east-1
+export AWS_PROFILE=default
+export JWT_SECRET="lets do-it"
+export URL=http://localhost:8100
+export AWS_ACCESS_KEY_ID= "AKIAXG5L54K3ZNGV2K3Z"
+export AWS_SECRET_ACCESS_KEY="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"
